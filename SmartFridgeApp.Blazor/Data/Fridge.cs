69b419e42c1e08dec9c7079836b8092c45c23850
Dariusz <dariusz151@gmail.com>
2020-10-01 23:39:53 +0200

    first attempt to blazor front end

---
+++ SmartFridgeApp.Blazor/Data/Fridge.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartFridgeApp.Blazor.Data
+{
+    public class Fridge
+    {
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+
+    }
+}
+
---
+++ SmartFridgeApp.Blazor/Data/FridgesService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SmartFridgeApp.Blazor.Data
+{
+    public class FridgesService
+    {
+        public async Task<Fridge[]> GetFridgesAsync()
+        {
+            HttpClient _httpClient;
+            _httpClient = new HttpClient();
+            var response = await _httpClient.GetAsync("https://localhost:5001/api/fridges");
+
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            
+            //return await JsonSerializer.DeserializeAsync
+            //    <IEnumerable<string>>(responseStream);
+
+            return await Task.FromResult(Enumerable.Range(1, 5).Select(index => new Fridge
+            {
+                Name = "siema",
+                Number = 1
+            }).ToArray());
+        }
+    }
+}
