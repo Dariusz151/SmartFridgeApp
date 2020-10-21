4a1e081ae79bff6176b9ca3a1c3a3597e1fd93cc
Dariusz <dariusz151@gmail.com>
2020-10-21 23:53:47 +0200

    fixed recipe views in database

--- SmartFridgeApp.IntegrationTests/FakeDatabases/FakeFoodProductRepository.cs
+++ SmartFridgeApp.IntegrationTests/FakeDatabases/FakeFoodProductRepository.cs
@@ -6,41 +6,41 @@ using System.Threading.Tasks;
 
 namespace SmartFridgeApp.IntegrationTests.FakeDatabases
 {
-    public class FakeFoodProductRepository : IFoodProductRepository
-    {
-        public Task AddAsync(FoodProduct foodProduct)
-        {
-            throw new NotImplementedException();
-        }
+    //public class FakeFoodProductRepository : IFoodProductRepository
+    //{
+        //public Task AddAsync(FoodProduct foodProduct)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Task CreateCategoryAsync(Category category)
-        {
-            throw new NotImplementedException();
-        }
+        //public Task CreateCategoryAsync(Category category)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Task DeleteAsync(int foodProductId)
-        {
-            throw new NotImplementedException();
-        }
+        //public Task DeleteAsync(int foodProductId)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Task<ICollection<FoodProduct>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+        //public Task<ICollection<FoodProduct>> GetAllAsync()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Task<IEnumerable<Category>> GetAllCategoriesAsync()
-        {
-            throw new NotImplementedException();
-        }
+        //public Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Task<FoodProduct> GetByIdAsync(int foodProductId)
-        {
-            throw new NotImplementedException();
-        }
+        //public Task<FoodProduct> GetByIdAsync(int foodProductId)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Task<Category> GetCategoryByIdAsync(int categoryId)
-        {
-            throw new NotImplementedException();
-        }
-    }
+        //public Task<Category> GetCategoryByIdAsync(int categoryId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    //}
 }
