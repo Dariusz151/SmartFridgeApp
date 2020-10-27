64f811ef1bcf0965e51b080af5eebafd1673839e
Dariusz <dariusz151@gmail.com>
2020-10-27 22:25:53 +0100

    little cleanup

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-
-# dotnet restore
-
-# dotnet build -c Release
--- pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-docker build -t dariusz151/smartfridgeapp:dev .
-
-#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-#docker push repository/project:$TAG
-#docker push repository/project:latest
