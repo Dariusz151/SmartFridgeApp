c701737192779183980822f7b0af5037731e0003
Dariusz <dariusz151@gmail.com>
2021-01-06 22:06:21 +0100

    handled adding new recipe from dialog

--- SmartFridgeApp.Web/smartfridgeapp-web/src/components/dialogs/NewRecipeDialog.js
+++ SmartFridgeApp.Web/smartfridgeapp-web/src/components/dialogs/NewRecipeDialog.js
@@ -20,31 +20,52 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const NewRecipeDialog = ({ state, handleClose }) => {
-  //   const [foodProducts, setFoodProducts] = useState([
-  //     { foodProductId: 1, foodProductName: "fp" },
-  //   ]);
-  //   const [foodProductId, setFoodProductId] = useState(0);
-  //   const [inputCount, incrementInputs] = useState(1);
-  const [foodProductInputs, addFoodProductInput] = useState([
-    {
-      input: (
-        <React.Fragment>
-          <TextField
-            name="input"
-            label="input"
-            fullWidth
-            //   onChange={(e) => {
-            //     setRecipeName(e.target.value);
-            //   }}
-            //   value={recipeName}
-          />
-        </React.Fragment>
-      ),
-    },
-  ]);
-
   const [recipeName, setRecipeName] = useState("");
   const [recipeDesc, setRecipeDesc] = useState("");
+  const [recipeCategory, setRecipeCategory] = useState("");
+  const [levelOfDifficulty, setDifficulty] = useState("");
+  const [requiredTime, setRequiredTime] = useState("");
+
+  const [foodProducts, setFoodProducts] = useState([
+    { foodProductId: 1, foodProductName: "fp" },
+  ]);
+  const [inputList, setInputList] = useState([
+    { foodProductId: 0, foodProductName: "", amount: "" },
+  ]);
+
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  const handleAddClick = () => {
+    setInputList([
+      ...inputList,
+      { foodProductId: 0, foodProductName: "", amount: "" },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log("food products fetch");
+    fetch(configData.SERVER_URL + "/api/foodProducts")
+      .then((response) => response.json())
+      .then((json) => {
+        const items = json.map((item, index) => ({
+          foodProductId: item.foodProductId,
+          foodProductName: item.foodProductName,
+        }));
+        setFoodProducts(items);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   //   useEffect(() => {
   //     // fetch(configData.SERVER_URL + "/api/recipes")
@@ -59,46 +80,57 @@ const NewRecipeDialog = ({ state, handleClose }) => {
   //     //   .catch((error) => console.error(error));
   //   }, []);
 
-  //   const refreshForm = () => {
-  //     setValues({
-  //       note: "",
-  //       unit: "",
-  //       value: "",
-  //     });
-  //     setFoodProductId(0);
-  //   };
+  const refreshForm = () => {
+    console.log("TODO: clear all form values.");
+  };
 
   const handleAdd = () => {
-    console.log("add");
-  };
+    const products = [];
+    inputList.map((item) => {
+      const newProduct = {
+        foodProductId: item.foodProductId,
+        amountValue: { value: parseInt(item.amount), unit: "NotAssigned" },
+      };
+      products.push(newProduct);
+      return true;
+    });
 
-  const handleAddNewInput = () => {
-    addFoodProductInput((oldArray) => [
-      ...oldArray,
-      {
-        input: (
-          <React.Fragment>
-            <TextField
-              name=""
-              label=""
-              fullWidth
-              //   onChange={(e) => {
-              //     setRecipeName(e.target.value);
-              //   }}
-              //   value={recipeName}
-            />
-          </React.Fragment>
-        ),
+    const obj = {
+      name: recipeName,
+      description: recipeDesc,
+      recipeCategory: parseInt(recipeCategory),
+      requiredTime: parseInt(requiredTime),
+      levelOfDifficulty: parseInt(levelOfDifficulty),
+      products: products,
+    };
+
+    console.log(obj);
+
+    fetch(configData.SERVER_URL + "/api/recipes/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
       },
-    ]);
-    //incrementInputs(inputCount + 1);
-  };
+      body: JSON.stringify(obj),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          toast.error("Cant add recipe!", {
+            position: "bottom-center",
+            autoClose: 1500,
+          });
+          throw Error(response.statusText);
+        }
+        toast.success("Added new recipe!", {
+          position: "bottom-center",
+          autoClose: 1500,
+        });
 
-  const handleDeleteInput = () => {
-    //const newArray = foodProductInputs;
-    //newArray.pop();
-    addFoodProductInput([]);
-    //addFoodProductInput(newArray);
+        return response;
+      })
+      .then(refreshForm())
+      .then(() => handleClose())
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -111,26 +143,60 @@ const NewRecipeDialog = ({ state, handleClose }) => {
         aria-labelledby="form-dialog-title"
       >
         <DialogTitle id="form-dialog-title">Add Recipe</DialogTitle>
-        <DialogActions>
-          <Button
-            onClick={handleDeleteInput}
-            color="primary"
-            variant="outlined"
-          >
-            <RemoveIcon />
-          </Button>
-          <Button
-            onClick={handleAddNewInput}
-            color="primary"
-            // startIcon={}
-            variant="outlined"
-          >
-            <AddIcon />
-          </Button>
-        </DialogActions>
+        <DialogActions></DialogActions>
         <DialogContent>
-          {foodProductInputs.map((item, index) => {
-            return <React.Fragment key={index}>{item.input}</React.Fragment>;
+          {inputList.map((x, i) => {
+            return (
+              <div className="box">
+                <Autocomplete
+                  id="foodproducts-combobox"
+                  options={foodProducts}
+                  getOptionLabel={(option) => option.foodProductName}
+                  // style={{ width: "40%" }}
+                  onChange={(e, val) => {
+                    const list = [...inputList];
+                    list[i].foodProductId = val.foodProductId;
+                    setInputList(list);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Select food product"
+                      variant="outlined"
+                    />
+                  )}
+                />
+                <TextField
+                  // style={{ width: "40%" }}
+                  // className="ml10"
+                  name="amount"
+                  placeholder="Enter Amount"
+                  value={x.amount}
+                  onChange={(e) => handleInputChange(e, i)}
+                />
+                <div className="btn-box">
+                  {inputList.length !== 1 && (
+                    <Button
+                      color="default"
+                      variant="contained"
+                      className="mr10"
+                      onClick={() => handleRemoveClick(i)}
+                    >
+                      <RemoveIcon />
+                    </Button>
+                  )}
+                  {inputList.length - 1 === i && (
+                    <Button
+                      color="default"
+                      variant="contained"
+                      onClick={handleAddClick}
+                    >
+                      <AddIcon />
+                    </Button>
+                  )}
+                </div>
+              </div>
+            );
           })}
           <TextField
             name="recipeName"
@@ -149,6 +215,58 @@ const NewRecipeDialog = ({ state, handleClose }) => {
               setRecipeDesc(e.target.value);
             }}
             value={recipeDesc}
+          />
+          <br />
+          <br />
+          <ToggleButtonGroup
+            value={levelOfDifficulty}
+            exclusive
+            onChange={(event, diff) => {
+              setDifficulty(diff);
+            }}
+            aria-label="text alignment"
+          >
+            <ToggleButton value="1" aria-label="left aligned">
+              <Typography>Easy</Typography>
+            </ToggleButton>
+            <ToggleButton value="2" aria-label="centered">
+              <Typography>Medium</Typography>
+            </ToggleButton>
+            <ToggleButton value="3" aria-label="right aligned">
+              <Typography>Hard</Typography>
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <br /> <br />
+          <ToggleButtonGroup
+            value={recipeCategory}
+            exclusive
+            onChange={(event, cat) => {
+              setRecipeCategory(cat);
+            }}
+            aria-label="text alignment"
+          >
+            <ToggleButton value="1" aria-label="centered">
+              <Typography>Breakfast</Typography>
+            </ToggleButton>
+            <ToggleButton value="2" aria-label="left aligned">
+              <Typography>Dinner</Typography>
+            </ToggleButton>
+
+            <ToggleButton value="3" aria-label="right aligned">
+              <Typography>Supper</Typography>
+            </ToggleButton>
+            <ToggleButton value="4" aria-label="right aligned">
+              <Typography>Other</Typography>
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <TextField
+            name="requiredTime"
+            label="Required time"
+            fullWidth
+            onChange={(e) => {
+              setRequiredTime(e.target.value);
+            }}
+            value={requiredTime}
           />
         </DialogContent>
 
