f50511d3a141b82d86699cb5e26e20099c536e3e
Dariusz <dariusz151@gmail.com>
2021-01-13 18:41:58 +0100

    Add styles and content in AdminLogin page

--- SmartFridgeApp.Web/smartfridgeapp-web/src/components/AdminLogin.js
+++ SmartFridgeApp.Web/smartfridgeapp-web/src/components/AdminLogin.js
@@ -1,39 +1,109 @@
 import React, { useState, useEffect } from "react";
 import configData from "../config_url.json";
+import { useHistory } from "react-router-dom";
+
+import { MDBTypography, MDBBtn } from "mdbreact";
+import { Container, Box, Grid, TextField } from "@material-ui/core";
 
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from "@material-ui/icons/Close";
-import ToggleButton from "@material-ui/lab/ToggleButton";
-import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
-import Typography from "@material-ui/core/Typography";
-import { Container, Box, Grid, TextField } from "@material-ui/core";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AdminLogin = () => {
+  let history = useHistory();
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
 
+  const refreshForm = () => {
+    setLogin("");
+    setPassword("");
+  };
+
+  const handleClose = () => {
+    history.push("/fridges");
+  };
+
+  const handleLogin = () => {
+    const obj = {
+      login: login,
+      password: password,
+    };
+    fetch(configData.SERVER_URL + "/api/auth", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          toast.error("Cant login to admin account!", {
+            position: "bottom-center",
+            autoClose: 1500,
+          });
+          throw Error(response.statusText);
+        }
+        toast.success("Logged in as admin!", {
+          position: "bottom-center",
+          autoClose: 1500,
+        });
+
+        return response;
+      })
+      .then(refreshForm())
+      .then(handleClose())
+      .catch((error) => console.log(error));
+  };
+
   return (
     <React.Fragment>
-      <p>Admin Login:</p>
-      <TextField
-        name="login"
-        label="Login"
-        fullWidth
-        onChange={(e) => {
-          setLogin(e.target.value);
-        }}
-        value={login}
-      />
-      <TextField
-        name="password"
-        label="Password"
-        fullWidth
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        value={password}
-      />
+      <ToastContainer />
+      <Container style={{ minHeight: "75vh", maxWidth: "50vw" }}>
+        <MDBTypography tag="h2" style={{ marginTop: "20px" }}>
+          Admin Login:
+        </MDBTypography>
+        <TextField
+          name="login"
+          label="Login"
+          fullWidth
+          onChange={(e) => {
+            setLogin(e.target.value);
+          }}
+          value={login}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          fullWidth
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+
+        <Button
+          onClick={handleClose}
+          color="secondary"
+          variant="outlined"
+          style={{ marginTop: "50px", marginRight: "10px" }}
+          startIcon={<CloseIcon />}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={handleLogin}
+          color="primary"
+          style={{ marginTop: "50px", marginRight: "10px" }}
+          startIcon={<AddIcon />}
+          variant="contained"
+        >
+          Login
+        </Button>
+      </Container>
     </React.Fragment>
   );
 };
