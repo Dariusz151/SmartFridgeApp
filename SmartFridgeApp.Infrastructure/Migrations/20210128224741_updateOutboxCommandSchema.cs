198eb61c431e529e7488fe388b6f92c82065ff1b
Dariusz <dariusz151@gmail.com>
2021-01-28 23:49:00 +0100

    Refactor in database scripts

---
+++ SmartFridgeApp.Infrastructure/Migrations/20210128224741_updateOutboxCommandSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartFridgeApp.Infrastructure.Migrations
+{
+    public partial class updateOutboxCommandSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "internal");
+
+            migrationBuilder.RenameTable(
+                name: "OutboxMessages",
+                schema: "app",
+                newName: "OutboxMessages",
+                newSchema: "internal");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "OutboxMessages",
+                schema: "internal",
+                newName: "OutboxMessages",
+                newSchema: "app");
+        }
+    }
+}
